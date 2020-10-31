5f4d8b83406226e94bd521be5a55da043e7fc8b5
Racing Icemen <rkkommu@ucdavis.edu>
2020-10-31 07:54:39 -0700

    Parameterization of Markov Chains

--- sources/markovchain.py
+++ sources/markovchain.py
@@ -2,14 +2,16 @@ import numpy as np
 
 
 class MarkovChain:
-    def __init__(self, transition_prob):
-        self.transition_prob = transition_prob
-        self.states = list(transition_prob.keys())
+    def __init__(self, transition_matrix, states):
+        self.transition_matrix = np.atleast_2d(transition_matrix)
+        self.states = states
+        self.index_dict = {self.states[index]: index for index in range(len(self.states))}
+        self.state_dict = {index: self.states[index] for index in range(len(self.states))}
 
     def next_state(self, current_state):
         return np.random.choice(
-            self.states, p=[self.transition_prob[current_state][next_state]
-                            for next_state in self.states])
+            self.states,
+            p=self.transition_matrix[self.index_dict[current_state], :])
 
     def generate_states(self, current_state, number=10):
         future_states = []
@@ -21,12 +23,12 @@ class MarkovChain:
 
 
 if __name__ == '__main__':
-    transition_prob = {
-        "Sunny": {"Sunny": 0.8, "Rainy": 0.19, "Snowy": 0.01},
-        "Rainy": {"Sunny": 0.2, "Rainy": 0.7, "Snowy": 0.1},
-        "Snowy": {"Sunny": 0.1, "Rainy": 0.2, "Snowy": 0.7},
-    }
-    weather_chain = MarkovChain(transition_prob=transition_prob)
+    transition_matrix = [
+        [0.8, 0.19, 0.01],
+        [0.2, 0.7, 0.1],
+        [0.1, 0.2, 0.7]
+    ]
+    weather_chain = MarkovChain(transition_matrix=transition_matrix, states=['Sunny', 'Rainy', 'Snowy'])
     print(weather_chain.next_state(current_state='Sunny'))
     print(weather_chain.next_state(current_state='Snowy'))
     print(weather_chain.generate_states(current_state='Snowy'))
